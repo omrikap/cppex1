93ada8df40a48cb334f2faa7f6170028ad944021
omrikap <omrikap@gmail.com>
2015-08-26 01:08:35 +0300

    added trace matrix

--- IntMatrix.cpp
+++ IntMatrix.cpp
@@ -33,11 +33,12 @@ IntMatrix::IntMatrix(IntMatrix &other) // toTest
 	}
 }
 
-IntMatrix::IntMatrix(const int _rows, const int _cols) // toTest
+IntMatrix::IntMatrix(const int rows, const int cols, int* matrixArray) // toTest
 {
-	this->_rows = _rows;
-	this->_cols = _cols;
-	this->_matrix = new int[_rows * _cols];
+	this->_rows = rows;
+	this->_cols = cols;
+	this->_matrix = new int[rows * cols];
+	this->_matrix = matrixArray;
 	cout << "++ a new matrix of " << this->_rows << " on " << this->_cols << endl; // todo remove
 }
 
@@ -53,41 +54,109 @@ IntMatrix::~IntMatrix() //toTest
 
 IntMatrix& IntMatrix::operator=(const IntMatrix &other) // toTest
 {
-	if (this == &other)
+	if (this != &other) // in case of self assignment, just return the same object.
 	{
-		return *this;
-	}
-	if (_matrix != nullptr)
-	{
-		if ((other._rows * other._cols) != (this->_rows * this->_cols))
+		if (_matrix != nullptr)
 		{
-			delete[] _matrix;
+			if ((other._rows * other._cols) != (this->_rows * this->_cols))
+			{
+				delete[] _matrix;
+				_matrix = new int[other._rows * other._rows];
+			}
+		}
+		else
+		{
 			_matrix = new int[other._rows * other._rows];
 		}
-	}
-	this->_rows = other._rows;
-	this->_cols = other._cols;
-	int matrixArrayLength = other._rows * other._rows;
-	for (int i = 0; i < matrixArrayLength; ++i)
-	{
-		_matrix[i] = other._matrix[i];
+		this->_rows = other._rows;
+		this->_cols = other._cols;
+		int matrixArrayLength = other._rows * other._rows;
+		for (int i = 0; i < matrixArrayLength; ++i)
+		{
+			_matrix[i] = other._matrix[i];
+		}
 	}
 	return *this;
 }
 
+IntMatrix &IntMatrix::operator+=(const IntMatrix &other) // toTest todo more checks?
+{
+	int matrixArrayLength = _rows * _cols;
+	for (int i = 0; i < matrixArrayLength; ++i)
+	{
+		_matrix[i] += other._matrix[i];
+	}
 
+	return *this;
+}
+
+IntMatrix &IntMatrix::operator-=(const IntMatrix &other) // toTest todo more checks?
+{
+	int matrixArrayLength = _rows * _cols;
+	for (int i = 0; i < matrixArrayLength; ++i)
+	{
+		_matrix[i] -= other._matrix[i];
+	}
+
+	return *this;
+}
+
+int IntMatrix::trace()
+{
+	int theTrace = 0;
+	if (_matrix != nullptr)
+	{
+		for (int i = 0; i < _rows; ++i)
+		{
+			theTrace += _matrix[i*_rows + i];
+		}
+	}
+	return theTrace;
+}
 
 // ---------------------------------- unit-testing --------------------------------------
 
 #ifndef NDEBUG
 
+void printMatrix(IntMatrix &matrix)
+{
+	for (int i = 0; i < matrix.get_rows(); ++i)
+	{
+		for (int j = 0; j < matrix.get_cols(); ++j)
+		{
+			cout << matrix.get_matrix()[i*matrix.get_cols() + j] << " ";
+		}
+		cout << endl;
+	}
+}
+
 int main()
 {
-	IntMatrix *M = new IntMatrix();
-	IntMatrix *S = new IntMatrix(2, 2);
+	int *firstArray = new int[4];
+	firstArray[0] = 2;
+	firstArray[1] = 4;
+	firstArray[2] = 1;
+	firstArray[3] = 3;
 
-	delete M;
-	delete S;
+	int *secArray = new int[4];
+	secArray[0] = 8;
+	secArray[1] = 3;
+	secArray[2] = 9;
+	secArray[3] = 7;
+	IntMatrix *S1 = new IntMatrix(2, 2, firstArray);
+
+	IntMatrix *S2 = new IntMatrix(2, 2, secArray);
+
+	IntMatrix& Sr = *S1;
+
+	printMatrix(Sr);
+	*S1 += *S2;
+
+	printMatrix(*S1);
+
+	cout << "the trace of S1: " << S1->trace() << endl;
+	delete S1;
+	delete S2;
 
 	return 0;
 }
--- IntMatrix.h
+++ IntMatrix.h
@@ -1,5 +1,5 @@
 #ifndef EX1_INTMATRIX_H
-	#define EX1_INTMATRIX_H
+#define EX1_INTMATRIX_H
 
 /**
  * A class that represent a matrix of integers.
@@ -28,8 +28,7 @@ public:
 	 * @param rows The number of rows in the matrix.
 	 * @param cols The number of columns in the matrix.
 	 */
-	IntMatrix(const int _rows, const int _cols);
-
+	IntMatrix(const int rows, const int cols, int *matrixArray);
 
 	/**
 	 * @brief The '=' operator.
@@ -37,10 +36,61 @@ public:
 	 */
 	IntMatrix& operator=(const IntMatrix& other);
 
+	/**
+	 * @brief IntMatrix compound addition operator.
+	 * @param other The matrix on the right hand side of the operator. Will be added to the matrix
+	 * 				on the left hand side.
+	 * @return A reference to the calling matrix (the matrix on the left hand side of the
+	 * 		   operator).
+	 */
+	IntMatrix& operator+=(const IntMatrix &other);
+
+	/**
+	 * @brief IntMatrix compound reduction operator.
+	 * @param other The matrix on the right hand side of the operator. Will be subtract from the
+	 * 	 		 	matrix on the left hand side.
+	 * @return A reference to the calling matrix (the matrix on the left hand side of the
+	 * 		   operator).
+	 */
+	IntMatrix& operator-=(const IntMatrix &other);
+
+	/**
+	 * @brief Calculate the trace of a square matrix. Behavior for non-sqare matrices is
+	 * 		  undefined.
+	 * @return int The trace of the calling matrix.
+	 */
+	int trace();
+
 private:
 	int _rows; /* the number of rows of the matrix. */
 	int _cols; /* the number of columns of the matrix. */
 	int *_matrix; /* A pointer to the int array of the matrix. */
+
+public:
+	int get_rows() const
+	{
+		return _rows;
+	}
+
+	void set_rows(int _rows)
+	{
+		IntMatrix::_rows = _rows;
+	}
+
+	int get_cols() const
+	{
+		return _cols;
+	}
+
+	void set_cols(int _cols)
+	{
+		IntMatrix::_cols = _cols;
+	}
+
+	int *get_matrix() const
+	{
+		return _matrix;
+	}
 };
 
 #endif //EX1_INTMATRIX_H
